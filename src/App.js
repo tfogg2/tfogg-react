fae86cb97c0b86e90cc6fbb165fb53532b876f26
tfogg2 <foggts@g.cofc.edu>
2018-08-27 19:29:40 -0400

    launch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Assets/App.css';
 import Home from './Components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Home />
       </div>
     );
