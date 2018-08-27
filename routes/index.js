64b03fc4787005e57f3cb79f57ae9608e2fa4c90
tfogg2 <foggts@g.cofc.edu>
2018-08-27 17:29:56 -0400

    launch

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,12 @@ const path = require('path')
 
 require('dotenv').config()
 
-app.use(express.static(path.join(__dirname, 'tfogg-react/build')))
+app.use(express.static(path.join(__dirname, 'tfogg/build')))
 
 app.use(require('prerender-node'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/tfogg-react/build/index.html'))
+  res.sendFile(path.join(__dirname + '/tfogg/build/index.html'))
 })
 
 
