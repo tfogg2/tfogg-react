8fc66d38e420d6726c50674affb957ca6b7f6d82
tfogg2 <foggts@g.cofc.edu>
2018-08-27 18:29:21 -0400

    launch

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'tfogg/build')))
+app.use(express.static(path.join(__dirname, 'tfogg-react/build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/tfogg/build/index.html'))
+  res.sendFile(path.join(__dirname + '/tfogg-react/build/index.html'))
 })
 
 const port = process.env.PORT || 5000
