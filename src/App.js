998ad196084b7fdcdc2952dfd1620b920f3dae97
tfogg2 <foggts@g.cofc.edu>
2018-08-27 19:37:09 -0400

    launch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './Assets/App.css';
 import Home from './Components/Home'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Home />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/" render={() => <Home />}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
