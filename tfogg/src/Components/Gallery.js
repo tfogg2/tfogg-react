cafff9af1593dd4ee4e187fb7016503f68e3b4d4
tfogg2 <foggts@g.cofc.edu>
2019-01-10 16:59:32 -0600

    new photos

--- tfogg/src/Components/Gallery.js
+++ tfogg/src/Components/Gallery.js
@@ -7,56 +7,64 @@ class Gallery extends Component {
 
     const images = [
       {
-        original: require('../Assets/profile-pic.svg'),
-        thumbnail: require('../Assets/profile-pic-small.png')
+        original: require('../Assets/kodi-shake.png'),
+        thumbnail: require('../Assets/kodi-shake-mini.png')
       },
       {
-        original: require('../Assets/kodi.svg'),
-        thumbnail: require('../Assets/kodi-small.png')
+        original: require('../Assets/mid-shack.png'),
+        thumbnail: require('../Assets/mid-shack-mini.png')
       },
       {
-        original: require('../Assets/camp.svg'),
-        thumbnail: require('../Assets/camp-small.png')
+        original: require('../Assets/medic.png'),
+        thumbnail: require('../Assets/medic-mini.png')
       },
       {
-        original: require('../Assets/maine.svg'),
-        thumbnail: require('../Assets/maine-small.png')
+        original: require('../Assets/kodi-xmas.png'),
+        thumbnail: require('../Assets/kodi-xmas-mini.png')
       },
       {
-        original: require('../Assets/snow.svg'),
-        thumbnail: require('../Assets/snow-small.png')
+        original: require('../Assets/dash.png'),
+        thumbnail: require('../Assets/dash-mini.png')
       },
       {
-        original: require('../Assets/nc-water.png'),
-        thumbnail: require('../Assets/nc-water-small.png')
+        original: require('../Assets/cat.png'),
+        thumbnail: require('../Assets/cat-mini.png')
       },
       {
-        original: require('../Assets/rosa.svg'),
-        thumbnail: require('../Assets/rosa-small.png')
+        original: require('../Assets/sky.png'),
+        thumbnail: require('../Assets/sky-mini.png')
       },
       {
-        original: require('../Assets/pup.svg'),
-        thumbnail: require('../Assets/pup-small.png')
+        original: require('../Assets/bridge.png'),
+        thumbnail: require('../Assets/bridge-mini.png')
       },
       {
-        original: require('../Assets/charleston-water.png'),
-        thumbnail: require('../Assets/charleston-water-small.png')
+        original: require('../Assets/virginia.png'),
+        thumbnail: require('../Assets/virginia-mini.png')
       },
       {
-        original: require('../Assets/kodi-eyes.png'),
-        thumbnail: require('../Assets/kodi-eyes-small.png')
+        original: require('../Assets/night-ski.png'),
+        thumbnail: require('../Assets/night-ski-mini.png')
       },
       {
-        original: require('../Assets/ford-air.svg'),
-        thumbnail: require('../Assets/ford-air-small.png')
+        original: require('../Assets/tree.png'),
+        thumbnail: require('../Assets/tree-mini.png')
       },
       {
-        original: require('../Assets/st-john-flight.png'),
-        thumbnail: require('../Assets/st-john-flight-small.png')
+        original: require('../Assets/bv_ski.png'),
+        thumbnail: require('../Assets/bv_ski-mini.png')
       },
       {
-        original: require('../Assets/sushi.png'),
-        thumbnail: require('../Assets/sushi-small.png')
+        original: require('../Assets/sip.png'),
+        thumbnail: require('../Assets/sip-mini.png')
+      },
+      {
+        original: require('../Assets/bv-friend.png'),
+        thumbnail: require('../Assets/bv-friend-mini.png')
+      },
+      {
+        original: require('../Assets/kodi-profile.png'),
+        thumbnail: require('../Assets/kodi-profile-mini.png')
       },
     ]
 
